a57b5daa884a5680bab76bf59c6b0d09ae9bb096
Mert Çalışkan <mert.caliskan@mynextmatch.com>
2020-08-18 14:40:22 +0300

    student model

--- Models/StudentData.cs
+++ Models/StudentData.cs
@@ -8,13 +8,18 @@ namespace StudentSystemWeb.Models
         public int Id { get; set; }
 
         [Required]
-        [MaxLength(50)]
+        [StringLength(50, ErrorMessage = "{0} length must be between {2} and {1}.", MinimumLength = 2)]
         public string Name { get; set; }
+
         [Required]
-        [MaxLength(50)]
+        [Display(Name = "Lastname")]
+        [StringLength(50, ErrorMessage = "{0} length must be between {2} and {1}.", MinimumLength = 2)]
         public string SurName { get; set; }
-        [Required]
-        [MaxLength(20)]
+
+        [Required(ErrorMessage = "You must provide a phone number")]
+        [Display(Name = "Phone")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"\(?\d{3}\)?-? *\d{3}-? *-?\d{4}", ErrorMessage = "Not a valid phone number")]
         public string Phone { get; set; }
         [Required]
         [MaxLength(100)]
